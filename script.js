78881cc8088acdbb2595c27f25db653d4d5096b2
Noctrya72 <alessiomaira2013@gmail.com>
2026-07-11 16:43:24 +0200

    Add files via upload
    Ajout de plusieurs stations et corrections de bugs.

--- script.js
+++ script.js
@@ -32,15 +32,15 @@ const stations = {
             plan: "https://pass.lesgets.com/wp-content/uploads/2025/10/SoleGets-PlanPistesH2025-2026.pdf"
         },
 
-        "Flaine": {
+        "Flaine (Grand Massif)": {
             lat: 46.007,
             lon: 6.694,
             url: "https://www.flaine.com",
             webcam: "https://www.flaine.com/webcams",
-            plan: "https://www.flaine.com/wp-content/uploads/2025/11/GM_PlanPistes_25_26_HD.pdf"
+            plan: "https://www.grand-massif.com/content/uploads/2025/12/GM_PlanPistes-1.pdf"
         },
 
-        "Samoëns": {
+        "Samoëns (Grand Massif)": {
             lat: 46.082,
             lon: 6.726,
             url: "https://www.samoens.com",
@@ -64,12 +64,12 @@ const stations = {
             plan: "https://www.alpedhuez.com/wp-content/uploads/2025/11/PLAN-DES-PISTES-HIVER-2025-2026.pdf"
         },
 
-        "Val Thorens (3 vallées)": {
+        "Val Thorens (3 Vallées)": {
             lat: 45.297,
             lon: 6.580,
             url: "https://www.valthorens.com",
             webcam: "https://www.valthorens.com/fr/webcams",
-            plan: "https://www.valthorens.com/app/uploads/2024/11/plan-val-thorens-orelle-zoom-legende-bd.pdf"
+            plan: "https://www.les3vallees.com/media/download/plan-courchevel.pdf"
         },
 
         "Tignes": {
@@ -80,12 +80,12 @@ const stations = {
             plan: "https://public.tignes.net/TIGNES.NET/BROCHURES/PDP_TIGNES_VALDISERE.pdf"
         },
 
-        "La Plagne": {
+        "La Plagne (Paradiski)": {
             lat: 45.506,
             lon: 6.677,
             url: "https://www.la-plagne.com",
             webcam: "https://www.la-plagne.com/sites/default/files/brochures/pdf/PlanPistesLaPlagneParadiski.pdf",
-            plan: "https://www.la-plagne.com/fr/plan-des-pistes"
+            plan: "https://www.la-plagne.com/sites/default/files/brochures/pdf/PlanPistesParadiski2026_compressed.pdf"
         },
         "Chamonix": {
             lat: 45.924,
@@ -94,15 +94,15 @@ const stations = {
             webcam: "https://www.chamonix.com/webcams",
             plan: "https://www.chamonix.com/sites/default/files/media/brochures/Plan_VALLEE_Hiver%281%29.pdf"
         },
-        "Megève": {
+        "Megève (Evasion Mont Blanc)": {
             lat: 45.857,
             lon: 6.617,
             url: "https://www.megeve.com",
             webcam: "https://www.megeve.com/webcams",
-            plan: "https://www.datocms-assets.com/146703/1734512761-plan_megeve_hiver.pdf?dl=plan_megeve_hiver.pdf"
+            plan: "https://forfait.megeve.com/hiver/wp-content/uploads/sites/7/2025/11/2025-Plan-Megeve-Evasion-basseDef.pdf"
         },
 
-        "Courchevel (3 vallées)": {
+        "Courchevel (3 Vallées)": {
             lat: 45.415,
             lon: 6.634,
             url: "https://www.courchevel.com",
@@ -110,12 +110,12 @@ const stations = {
             plan: "https://www.les3vallees.com/media/download/plan-courchevel.pdf"
         },
 
-        "Les Arcs": {
+        "Les Arcs (Paradiski)": {
             lat: 45.572,
             lon: 6.827,
             url: "https://www.lesarcs.com",
             webcam: "https://www.lesarcs.com/webcams",
-            plan: "https://www.lesarcs.com/uploads/pics/plan-pistes-paradiski-2019_01.pdf"
+            plan: "https://www.la-plagne.com/sites/default/files/brochures/pdf/PlanPistesParadiski2026_compressed.pdf"
         },
 
         "Serre Chevalier": {
@@ -166,7 +166,7 @@ const stations = {
             plan: "https://www.sja73.com/medias/images/info_pages/plan-des-pistes-les-sybelles-2025-2026-969.pdf",
         },
 
-        "Les Saisies (espace diamant)": {
+        "Les Saisies (Espace Diamant)": {
             lat: 45.758800,
             lon: 6.535506,
             url: "https://www.lessaisies.com/",
@@ -221,7 +221,126 @@ const stations = {
             webcam: "https://www.valmorel.com/webcams/",
             plan: "https://skipass.valmorel.com/media/download/valmorelb2c/cms/media/HIVER/Plan%20des%20pistes/plan%20des%20pistes%2025-26%20complet.pdf",
         },
-    
+
+        "Méribel (3 Vallées)": {
+            lat: 45.391269,
+            lon: 6.567644,
+            url: "https://www.meribel.net/",
+            webcam: "https://www.meribel.net/informations-pratiques/webcams/",
+            plan: "https://www.les3vallees.com/media/download/plan-courchevel.pdf",
+        },
+
+        "Risoul": {
+            lat: 44.621036,
+            lon: 6.633803,
+            url: "https://www.risoul.com/hiver",
+            webcam: "https://www.risoul.com/webcams.html",
+            plan: "https://www.vars.com/app/uploads/vars/2025/11/VARS-Pistes2025-26BD.pdf",
+        },
+
+        "La rosière": {
+            lat: 45.628947,
+            lon: 6.848758,
+            url: "https://www.larosiere.net/",
+            webcam: "https://www.larosiere.net/webcams/",
+            plan: "https://www.cgh-residences.com/documents/plan-pistes/plan-pistes-rosiere.pdf",
+        },
+
+        "Sainte-Foy-Tarentaise": {
+            lat: 45.577272,
+            lon: 6.893675,
+            url: "https://www.saintefoy-tarentaise.com/",
+            webcam: "https://www.saintefoy-tarentaise.com/infos-live/webcam/",
+            plan: "https://www.saintefoy-tarentaise.com/app/uploads/saintefoy-tarentaise/2024/09/PLAN-DES-PISTES_SAINTE-FOY-TARENTAISE_HIVER-2023-2024.pdf",
+        },
+
+        "Peisey-Vallandry (Paradiski)": {
+            lat: 45.551036,
+            lon: 6.763722,
+            url: "https://www.peisey-vallandry.com/",
+            webcam: "https://www.peisey-vallandry.com/les-webcams.html",
+            plan: "https://www.la-plagne.com/sites/default/files/brochures/pdf/PlanPistesParadiski2026_compressed.pdf",
+        },
+
+        "Vars (Espace Forêt Blanche)": {
+            lat: 44.572789,
+            lon: 6.679261,
+            url: "https://www.vars.com/hiver/",
+            webcam: "https://www.vars.com/hiver/webcams/",
+            plan: "https://www.vars.com/app/uploads/vars/2025/11/VARS-Pistes2025-26BD.pdf",
+        },
+
+        "Les Menuires (3 Vallées)": {
+            lat: 45.323881,
+            lon: 6.538708,
+            url: "https://lesmenuires.com/fr/",
+            webcam: "https://lesmenuires.com/fr/direct?ref=shortcut",
+            plan: "https://www.les3vallees.com/media/download/plan-courchevel.pdf",
+        },
+
+        "Pra Loup": {
+            lat: 44.368714,
+            lon: 6.601250,
+            url: "https://www.praloup.com/",
+            webcam: "https://www.praloup.com/webcams/",
+            plan: "https://www.praloup.com/wp-content/uploads/2025/09/plan-des-pistes-de-pra-loup.pdf",
+        },
+
+        "Isola 2000": {
+            lat: 44.183728,
+            lon: 7.159794,
+            url: "https://isola2000.com/",
+            webcam: "https://www.stationsnicecotedazur.com/fr/webcam/isola/",
+            plan: "https://isola2000.com/wp-content/uploads/2023/10/isola-2000-v10-2023-2024-hd.pdf",
+        },
+
+        "Auron": {
+            lat: 44.220353,
+            lon: 6.925461,
+            url: "https://hiver.auron.com/",
+            webcam: "https://www.stationsnicecotedazur.com/fr/webcam/auron/",
+            plan: "https://anmsm.media.tourinsoft.eu/upload/AURON-Plans-des-PIstes-2025-2026--1-.pdf",
+        },
+
+        "Les Contamines-Montjoie (Evasion Mont Blanc)": {
+            lat: 45.802389,
+            lon: 6.712131,
+            url: "https://www.lescontamines.net/",
+            webcam: "https://www.lescontamines.net/webcam.html",
+            plan: "https://forfait.megeve.com/hiver/wp-content/uploads/sites/7/2025/11/2025-Plan-Megeve-Evasion-basseDef.pdf",
+        },
+
+        "Morillon (Grand Massif)": {
+            lat: 46.065292,
+            lon: 6.671522,
+            url: "https://www.grand-massif.com/les-5-stations-grand-massif/station-ski-morillon/",
+            webcam: "https://www.grand-massif.com/webcams/#Morillon",
+            plan: "https://www.grand-massif.com/content/uploads/2025/12/GM_PlanPistes-1.pdf",
+        },
+
+        "Saint-Gervais-Mont-Blanc (Evasion Mont Blanc)": {
+            lat: 45.874283,
+            lon: 6.685378,
+            url: "",
+            webcam: "",
+            plan: "https://forfait.megeve.com/hiver/wp-content/uploads/sites/7/2025/11/2025-Plan-Megeve-Evasion-basseDef.pdf",
+        },
+
+        "Praz-sur-Arly (Espace Daiamant)": {
+            lat: 45.833608,
+            lon: 6.570992,
+            url: "https://www.prazsurarly.com/fr",
+            webcam: "https://www.prazsurarly.com/fr/webcams",
+            plan: "https://www.lessaisies.com/app/uploads/les-saisies/2024/01/espace-diamant-hiver-2023-2024_HD.pdf",
+        },
+
+        "Les Carroz (Grand Massif)": {
+            lat: 46.024956,
+            lon: 6.644792,
+            url: "",
+            webcam: "https://www.grand-massif.com/webcams/#les-carroz",
+            plan: "https://www.grand-massif.com/content/uploads/2025/12/GM_PlanPistes-1.pdf",
+        },
     },
 
     Pyrénées: {
@@ -286,6 +405,14 @@ const stations = {
             url: "https://piau-engaly.com/",
             webcam: "https://piau-engaly.com/webcams-live/",
             plan: "https://piau-engaly.com/wp-content/uploads/2024/11/Plan-des-pistes-seul-2024-25.pdf",
+        },
+
+        "Guzet": {
+            lat: 42.778383,
+            lon: 1.301042,
+            url: "https://www.guzet.ski/fr/",
+            webcam: "https://www.guzet.ski/fr/webcam",
+            plan: "https://abonnement.guzet.ski/media/upload/guzetppu/cms/media/base_station/Plan%20pistes%20-%20Guzet_compressed.pdf",
         },
     },
 
